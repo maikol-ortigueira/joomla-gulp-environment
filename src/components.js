11375ac4d11a7b72f25413c5f1f20871268511c4
maikol-ortigueira <maikol.ortigueira@gmail.com>
2020-03-15 13:22:40 +0100

    Ya puedo empaquetar mi componente

--- src/components.js
+++ src/components.js
@@ -4,6 +4,7 @@ const copy = require('./components/copy.js');
 const watch = require('./components/watch.js');
 const newComponent = require('./components/new-component.js');
 const newView = require('./components/new-view.js');
+const release = require('./components/release.js');
 const { defaultTasks } = require('../gulp-config.json');
 
 
---
+++ src/components/release.js
@@ -0,0 +1,24 @@
+// Gulp
+const { src, dest, task, series } = require('gulp');
+
+// Config variables
+const { sourceDir, releaseDir, extTypes, joomlaVersion, extName, header } = require('../../gulp-config.json');
+
+// Zip compress files
+const zip = require('gulp-zip');
+
+// File systems
+fs = require('file-system');
+path = require('path');
+
+// Extension path
+extPath = path.join(sourceDir, extTypes, extName);
+
+task("release", ()=> {
+	return src([
+		path.join(extPath,'**'),
+		'!' + path.join(extPath,'docs/**')
+	])
+	.pipe(zip('com_' + extName + '_' + joomlaVersion + '_v' + header.version + '.zip'))
+	.pipe(dest('./tmp'));
+})
